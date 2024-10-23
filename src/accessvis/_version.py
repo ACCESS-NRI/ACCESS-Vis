0843103f1e9577c7908e880009477ae4ac5ef5bd
rbeucher <romain.beucher@anu.edu.au>
2024-10-23 11:35:17 +1000

    Small change

--- src/accessvis/_version.py
+++ src/accessvis/_version.py
@@ -51,7 +51,7 @@ def get_config() -> VersioneerConfig:
     cfg = VersioneerConfig()
     cfg.VCS = "git"
     cfg.style = "pep440"
-    cfg.tag_prefix = ""
+    cfg.tag_prefix = "v"
     cfg.parentdir_prefix = "accessvis-"
     cfg.versionfile_source = "src/accessvis/_version.py"
     cfg.verbose = False
