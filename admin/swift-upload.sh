293f89b34a22ad6446c9b8e305ba9e726bfd8d6b
Owen Kaluza <owen.kaluza@monash.edu>
2025-05-13 16:44:19 +1000

    Missed some files in upload script

--- admin/swift-upload.sh
+++ admin/swift-upload.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 #1) Download open-rc from nectar dashboard
 #2) Set password in nectar dashboard -> Settings
+#3) Enter password on prompt
 source access-nri-store-openrc.sh
 pip install python-swiftclient python-keystoneclient
-#
-#swift list
 
-#Fixing flipped jpegs
-#sudo apt install libjpeg-turbo-progs
-#mkdir tmp
-#mv *.jpg tmp
-#cd tmp
-#for i in *.jpg; do jpegtran -flip vertical $i > ../$i; done
 pushd /media/data/accessvis
 URL=https://object-store.rc.nectar.org.au/v1/
 swift --os-storage-url ${URL}AUTH_${OS_PROJECT_ID} upload accessvis bluemarble/cubemap_2048
 swift --os-storage-url ${URL}AUTH_${OS_PROJECT_ID} upload accessvis bluemarble/cubemap_4096
 swift --os-storage-url ${URL}AUTH_${OS_PROJECT_ID} upload accessvis bluemarble/cubemap_8192
 swift --os-storage-url ${URL}AUTH_${OS_PROJECT_ID} upload accessvis bluemarble/cubemap_16384
-#swift --os-storage-url ${URL}AUTH_${OS_PROJECT_ID} upload accessvis bluemarble/source_full
+swift --os-storage-url ${URL}AUTH_${OS_PROJECT_ID} upload accessvis bluemarble/source_full
 swift --os-storage-url ${URL}AUTH_${OS_PROJECT_ID} upload accessvis relief
 swift --os-storage-url ${URL}AUTH_${OS_PROJECT_ID} upload accessvis landmask
 swift --os-storage-url ${URL}AUTH_${OS_PROJECT_ID} upload accessvis gebco/*.npz
